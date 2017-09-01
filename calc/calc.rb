03788054a17980719279fa286c16be0b12e60d02
JHM <yerriel96@gmail.com>
2017-09-01 09:35:31 +0900

    JHM calc_tes.rb commit

---
+++ calc/calc.rb
@@ -0,0 +1,8 @@
+# show Number 3
+puts 3
+
+# return real number
+puts 9.0 / 2.0
+
+# return integer
+puts 9 / 2
---
+++ calc/calc_test.rb
@@ -0,0 +1,6 @@
+# Q. change a year to hours
+puts "1 year : 365 days"
+# time(Integer)
+time = 365 * 24
+# string + integer.to_s[change a integer to string] + string
+puts "365 days : " + time.to_s + " hours"
